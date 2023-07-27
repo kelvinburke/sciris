7015220ebf23cbd59eee5c3612a6557f31962b96
cliffckerr <questionably@gmail.com>
2023-07-26 23:27:53 -0400

    replace original init

--- sciris/__init__.py
+++ sciris/__init__.py
@@ -4,6 +4,9 @@ Sciris root module
 Typically just handles imports, but also sets number of threads for Numpy if SCIRIS_NUM_THREADS is set (see :class:`sc.options <sc_settings.ScirisOptions>`).
 """
 
+from time import time
+start = time()
+
 # Handle threadcount -- may require Sciris to be imported before Numpy; see https://stackoverflow.com/questions/17053671/how-do-you-stop-numpy-from-multithreading
 import os as _os
 _threads = _os.getenv('SCIRIS_NUM_THREADS', '')
@@ -20,22 +23,39 @@ if _threads: # pragma: no cover
 _lazy = _os.getenv('SCIRIS_LAZY', False)
 
 # Otherwise, import everything
+print(f'preliminaries: {time()-start}')
 if not _lazy:
     from .sc_version    import *
+    print(f'sc_version: {time()-start}')
     from .sc_utils      import *
+    print(f'sc_utils: {time()-start}')
     from .sc_printing   import *
+    print(f'sc_printing: {time()-start}')
     from .sc_nested     import *
+    print(f'sc_nested: {time()-start}')
     from .sc_odict      import *
+    print(f'sc_odict: {time()-start}')
     from .sc_settings   import *
+    print(f'sc_settings: {time()-start}')
     from .sc_datetime   import *
+    print(f'sc_datetime: {time()-start}')
     from .sc_math       import *
+    print(f'sc_math: {time()-start}')
     from .sc_dataframe  import *
+    print(f'sc_dataframe: {time()-start}')
     from .sc_fileio     import *
+    print(f'sc_fileio: {time()-start}')
     from .sc_versioning import *
+    print(f'sc_versioning: {time()-start}')
     from .sc_profiling  import *
+    print(f'sc_profiling: {time()-start}')
     from .sc_parallel   import *
+    print(f'sc_parallel: {time()-start}')
     from .sc_asd        import *
+    print(f'sc_asd: {time()-start}')
     from .sc_plotting   import *
+    print(f'sc_plotting: {time()-start}')
     from .sc_colors     import *
+    print(f'sc_colors: {time()-start}')
 
 del _os, _lazy, _threads
